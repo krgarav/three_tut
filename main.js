6afaebef11bc57c2e5cc9876d6531af01fdb5468
krgarav <krgarav@gmail.com>
2024-09-20 17:05:44 +0530

    Controls added to cube

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 import * as THREE from 'three';
+import {OrbitControls} from "three/addons/controls/OrbitControls.js"
 const scene = new THREE.Scene();
 
 const cubeGeometry = new THREE.BoxGeometry(1,1,1)
-const cubeMaterial =  new THREE.MeshBasicMaterial({color:"red"});
+const cubeMaterial =  new THREE.MeshBasicMaterial({color:"green"});
 const cubeMesh = new THREE.Mesh(cubeGeometry,cubeMaterial);
 scene.add(cubeMesh)
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/innerHeight,0.1,30);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/innerHeight,0.9,200
+);
 
-camera.position.z=5
+camera.position.z=3
 scene.add(camera)
 
 const canvas =  document.querySelector("canvas.threejs");
 const renderer = new THREE.WebGLRenderer({canvas});
-console.log(canvas);
 renderer.setSize(window.innerWidth,window.innerHeight)
-renderer.render(scene,camera)
+const controls = new OrbitControls(camera,canvas);
+controls.enableDamping=true;
+controls.autoRotate= true;
+const renderLoop = ()=>{
+ controls.update()
+  renderer.render(scene,camera)
+  window.requestAnimationFrame(renderLoop)
+}
+renderLoop();
+console.log(canvas);
+
